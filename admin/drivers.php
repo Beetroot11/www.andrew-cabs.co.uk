c7fd5d9093bd385d6877eec9d7a3be095cd6f7b4
Andrew Misselbrook <andrew@andrewtech.co.uk>
2021-10-27 18:31:32 +0100

    Prep for driver profiles

--- admin/drivers.php
+++ admin/drivers.php
@@ -9,10 +9,24 @@
 		<? include '../menu.php' ?>
 
 		<div class="content">
-			<div id="loginWindow">
-				Something
+			<div class="cards">
+				<div class="card">
+					Img
+					<div class="container">
+						<h4><b>John Doe</b></h4> 
+						<p>Architect & Engineer</p> 
+					</div>
+				</div>
+
+				<div class="card">Add New Driver</div>
 			</div>
 		</div>
 		<? include '../footer.php' ?>
+
+		<script>
+
+
+		</script>
+
 	</body>
 </html>
