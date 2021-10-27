c2427d284ba6c3db9d20f8fddb9425e8fd477078
Andrew Misselbrook <andrew@andrewtech.co.uk>
2021-10-27 21:04:39 +0100

    Update book.php

--- user/book.php
+++ user/book.php
@@ -35,21 +35,52 @@
 					</div>
 				</div>
 				<div id="bookingWhereInput">
-					<div class="inputTitle">Where do you want to go?</div>
+					<div class="inputTitle">Where do you want to go from?</div>
 					<div class="input">
 						<input type="text" 
-							id="booking-where" 
+							id="booking-where-from" 
 							class="formInput"
-							placeholder="Where do you want to be taken" 
-							aria-label="Where do you want to be taken" 
+							placeholder="Where we going from?" 
+							aria-label="Where do you want to be taken from" 
 							value="" 
 							autocomplete="off" />
-						<span id="bookingWhereError" class="inputError"></span>
+						<span id="bookingWhereFromError" class="inputError"></span>
 					</div>
+				</div>
+				<div id="bookingToInput">
+					<div class="inputTitle">Where do you want to go to?</div>
+					<div class="input">
+						<input type="text" 
+							id="booking-where-to" 
+							class="formInput"
+							placeholder="Where we going to?" 
+							aria-label="Where do you want to be taken to" 
+							value="" 
+							autocomplete="off" />
+						<span id="bookingWhereToError" class="inputError"></span>
+					</div>
+				</div>
+
+				<div id="bookBtn" class="btn">
+					<i class="fas fa-sign-in-alt"></i> Book
 				</div>
 			</div>
 		</div>
 
 		<? include '../footer.php' ?>
+
+		<script>
+			document.getElementById("bookBtn").addEventListener("click", login);
+
+			function book() {
+				var amountOfPeople = document.getElementById("booking-amount").value;
+				var when = document.getElementById("booking-when").value;
+				var where = document.getElementById("booking-where").value;
+				var to = document.getElementById("booking-to").value;
+
+				alert('booked!');
+			}
+
+		</script>
 	</body>
 </html>
