675b1274288521bcca4c8643176f42c5ff983cb9
Andrew Misselbrook <andrew@andrewtech.co.uk>
2021-10-27 20:45:57 +0100

    Vehicle

--- admin/vehicle.php
+++ admin/vehicle.php
@@ -9,8 +9,19 @@
 		<? include '../menu.php' ?>
 
 		<div class="content">
-			<div id="loginWindow">
-				Something
+			<div class="cards">
+				<div class="card">
+					Img
+					<div class="container">
+						<h4><b>Vehicle Make and Model</b></h4> 
+						<p>Some information about the car</p> 
+					</div>
+				</div>
+
+				<div id="newVehicle" class="card">
+					<div id="icon"><i class="fas fa-car fa-2x"></i></div>
+					<div id="desc">Add New Car</div>
+				</div>
 			</div>
 		</div>
 		<? include '../footer.php' ?>
