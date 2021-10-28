88e70ef970a27460f31902e4d3b31b44b24dc826
Andrew Misselbrook <andrew@andrewtech.co.uk>
2021-10-28 17:39:51 +0100

    added datatables

--- header.php
+++ header.php
@@ -4,9 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="https://www.andrew-cabs.co.uk/assets/css/main.css" />
     <link rel="stylesheet" href="https://www.andrew-cabs.co.uk/assets/css/dashboard.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
     <link rel="image_src" href="https://www.andrew-cabs.co.uk/favicon.ico" />
     <link rel="SHORTCUT ICON" href="https://www.andrew-cabs.co.uk/favicon.ico" />
     <script defer src="https://www.andrew-cabs.co.uk/assets/fontawesome/js/all.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.all.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 </head>
