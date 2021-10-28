ae0104508e27ac887f598abaff7664497b815635
Andrew Misselbrook <andrew@andrewtech.co.uk>
2021-10-28 18:22:38 +0100

    Update header.php

--- header.php
+++ header.php
@@ -8,7 +8,8 @@
     <link rel="image_src" href="https://www.andrew-cabs.co.uk/favicon.ico" />
     <link rel="SHORTCUT ICON" href="https://www.andrew-cabs.co.uk/favicon.ico" />
     <script defer src="https://www.andrew-cabs.co.uk/assets/fontawesome/js/all.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.all.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 </head>
